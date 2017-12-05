3327906c539c23e79dfc003b89e36187b4da2569
ezbanab <ezbanab@R90BWAB5.ericsson.se>
2017-12-05 12:47:01 +0530

    x

---
+++ src/CreateKeySpace.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Basics;
+
+/**
+ * @author ezbanab
+ *
+ */
+public class CreateKeySpace {
+
+	/**
+	 * 
+	 */
+	public CreateKeySpace() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
